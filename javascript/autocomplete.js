1288c8cc7616b36d74a0566ba5b8bf35867b9107
Martin Herbst <herbst.mrtn@gmail.com>
2025-12-06 15:01:15 +0100

    prevod naseptavace do OOP
    prevod naseptavace do OOP + zmenseni datasetu json souboru pro rychlejsi nacitani

--- javascript/autocomplete.js
+++ javascript/autocomplete.js
@@ -1,80 +1,91 @@
 
-//Nacteni mest
-/* ************************************************************* */
-async function loadCities() {
+
+class Cities {
     /*
-    Funkce ktera nacte data z json souboru do promenne cities
+    Trida zodpovedna za nacitany dat z json souboru
     */
-  const response = await fetch("../weather_data/city.list.json")
-  const cities = await response.json();
-  console.log("Města načtena:", cities.length);
-  return cities;
-}
-let allCities = [];
-loadCities().then(cities => {
-    allCities = cities;
-});
-/* ************************************************************* */
-
-
-
-//Filtrovani vysledku podle vstupu
-/* ************************************************************* */
-const resultBox = document.querySelector(".result-box");
-const inputBox = document.getElementById("input-box"); 
-
-inputBox.onkeyup = function() {
-    /*
-    Funkce, ktera reaguje na vstup, podle ktere filtruje odpovidajici vysledky
-    */
-    let result = [];
-    let input = inputBox.value;
-
-    if(input.length){
-        
-        result = allCities.filter(city => {
-            return city.name.toLowerCase().includes(input.toLowerCase()); //obsahuje jmeno mesta nas input? + osetreni case sensitive
-        });
-        result = result.map(city => city.name); //chceme jen nazvy mest, ne cele objekty
+    constructor(cityData) {
+        this.cityData = cityData;
+        this.cities = [];
     }
-    //Pole vhodnych nazvu mest posilan na vykresleni do naseptavace
-    display(result);
-};
-/* ************************************************************* */
+
+    async loadCities() {
+        const response = await fetch(this.cityData);
+        this.cities = await response.json();
+
+        return this.cities;
+    }
+}
 
 
 
-//Vykresleni vysledku naseptavace
-/* ************************************************************* */
-function display(result){
+class Autocomplete {
     /*
-    Funkce pro vykresleni naseptavace s jednim argumentem result,
-    ktery je pole validnich nazvu mest
+    Trida zodpovedna za logiku naseptavace
     */
+    constructor(inputBox, resultBox , allCities) {
+        this.inputBox = document.getElementById(inputBox);
+        this.resultBox = document.querySelector(resultBox);
+        this.allCities = allCities;
 
-    resultBox.innerHTML = ""; //zresetovani vysledku naseptavace - odstraneni kontentu uvnitr elementu
+        this.inputBox.addEventListener("keyup", () => this.onkeyUp());
+    }
 
-    //seznam valiodnich mest jako seznam
-    const validCitiesList = document.createElement("ul");
+    onkeyUp() {
+        /*
+        Funkce, ktera se vola pri psani znaku do inputBox
+        */
+        let result = []; //pole pro uchovani nazvu mest
+        let input = this.inputBox.value;
 
-    result.forEach(cityName => {
-        //validni mesto jako radek/cast seznamu
-        const validCity = document.createElement("li"); 
+        if(input.length) {
+            //filtrace mest podle nazvu, ktere odpovidaji hodnote v naseptavaci
+            result = this.allCities.filter(city => {
+                return city.name.toLowerCase().includes(input.toLowerCase());
+            });
 
-        //vlozeni nazvu mesta do radku
-        validCity.textContent = cityName;
+            //nakonec se ulozi jen nazvy mest, ne cele objekty
+            result = result.map(city => city.name);
 
-        //kdyz se klikne na tento radek, vlozi se kontent tohoto radku do hledacku a smaze se naseptavac
-        validCity.addEventListener("click", function() {
-            inputBox.value = cityName;      
-            resultBox.innerHTML = ""; 
+            this.display(result);
+        }
+    }
+
+    display(result) {
+        /*
+        Funkce pro samotne vypsani seznamu vhodnych mest
+        */
+
+        this.resultBox.innerHTML = ""; //zresetovani vysledku naseptavace - odstraneni kontentu uvnitr elementu
+
+        //seznam validnich mest jako seznam
+        const validCitiesList = document.createElement("ul");
+
+        result.forEach(cityName => {
+            //validni mesto jako radek/cast seznamu
+            const validCity = document.createElement("li"); 
+
+            //vlozeni nazvu mesta do radku
+            validCity.textContent = cityName;
+
+            //kdyz se klikne na tento radek, vlozi se kontent tohoto radku do hledacku a smaze se nabidka naseptavace
+            validCity.addEventListener("click", () => {
+                this.inputBox.value = cityName;      
+                this.resultBox.innerHTML = ""; 
+            });
+
+            //pridani validniho mesta do seznamu/vlozeni dalsiho radku do seznamu
+            validCitiesList.appendChild(validCity);
         });
 
-        //pridani validniho mesta do seznamu/vlozeni dalsiho radku do seznamu
-        validCitiesList.appendChild(validCity);
-    });
-
-    //vlozeni celeho seznamu do elementu
-    resultBox.appendChild(validCitiesList);
+        //vlozeni celeho seznamu do elementu
+        this.resultBox.appendChild(validCitiesList);
+    }
 }
-/* ************************************************************* */
+
+
+const allCities = new Cities("../city_data/city.list.json");
+allCities.loadCities().then(function() {
+    //cekani na nacteni mest
+    const autocomplete = new Autocomplete("input-box", ".result-box", allCities.cities);
+});
