35af18d9b5e2dba9288a41a224be9129b05e93db
Martin Herbst <herbst.mrtn@gmail.com>
2025-12-07 21:14:30 +0100

    zacatek vypisovani pocasi k jednotlivym dnum
    Zacatek logiky vypisovan pocasi k jednotlivym dnum - zatim nefunguje, nekde chyba

--- javascript/weather-renderer.js
+++ javascript/weather-renderer.js
@@ -5,6 +5,7 @@ export class WeatherRenderer {
     */
     constructor (weatherOutput) {
         this.weatherOutput = document.querySelector(weatherOutput + " .wrapper");
+        this.outputTargets = ["day-one", "day-two", "day-three", "day-four", "day-five"];
     }
     renderData(weatherData) {
         /*
@@ -14,17 +15,41 @@ export class WeatherRenderer {
         this.resetContent();
         this.createHeading();
 
+        let count = 0;
+        let dayIndex = 0;
+
+        for(let i=0;i<weatherData.list.length;i++) {
+            count++;
+            if(count == 8) {
+                dayIndex++;
+                count = 0;
+            }
+            this.printData(this.outputTargets[dayIndex], weatherData.list[i].dt)
+
+        }
+
     }
+    
+    printData(element, data) {
+        let p = document.createElement("p");
+        let target = document.getElementById(element);
+        p.textContent = data;
+        target.appendChild(p);
+    }
+        
     createHeading() {
         /*
         Metoda pro vytvoreni nadpisu, nestaci informace o nazvu mesta z weatherData, jelikoz nektera mesta nejsou pojmenovana zcela spravne
         */
         let cityName = document.getElementById("input-box").value;
         let h = document.createElement("h1");
-        h.textContent = "Počasí v " + cityName;
+        h.textContent = "Počasí " + cityName;
         this.weatherOutput.appendChild(h);
     }
     resetContent() {
+        /*
+        Vymazani obsahu
+        */
         this.weatherOutput.innerHTML = "";
     }
 }
